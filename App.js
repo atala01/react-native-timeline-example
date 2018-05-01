dab94ff327f31ef1c4bccad4d6efe0975bbbe881
Baris Ceviz <me@barisceviz.com>
2018-05-01 22:55:54 +0300

    Changed Package and project name

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ import {
   StyleSheet,
   Text,
   View,
-  ImageBackground,
-  Dimensions
+  Dimensions,
+  StatusBar
 } from 'react-native';
 import Timeline from 'react-native-timeline'
 let { width, height } = Dimensions.get("window");
@@ -265,7 +265,9 @@ export default class App extends Component {
     ]
     this.state = {
       selected: null,
-      color: '#E84C3C'
+      timeColor: this.getRandomColor(),
+      backgroundColor: this.getRandomColor(),
+      containerColor: this.getRandomColor()
     }
   }
 
@@ -281,38 +283,51 @@ export default class App extends Component {
   }
 
   pageChanged(pageNumber) {
-    let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    let backgroundColor = this.getRandomColor();
+    let containerColor = this.getRandomColor();
+    let timeColor = this.getRandomColor();
     this.setState({
-      color: color
+      backgroundColor: backgroundColor,
+      containerColor: containerColor,
+      timeColor: timeColor
     })
+  }
+
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
   render() {
     return (
-      <View style={[styles.container,{
-        backgroundColor: this.state.color
+      <View style={[styles.container, {
+        backgroundColor: this.state.backgroundColor
       }]}>
-          <Timeline
-            style={styles.list}
-            data={this.data}
-            circleSize={20}
-            circleColor='rgba(0,0,0,0)'
-            lineColor='rgb(45,156,219)'
-            timeContainerStyle={{ minWidth: 52, marginTop: -5 }}
-            timeStyle={{ textAlign: 'center', backgroundColor: '#ff9797', color: 'white', padding: 5, borderRadius: 13 }}
-            descriptionStyle={{ color: 'gray' }}
-            pageChanged={this.pageChanged.bind(this)}
-            options={{
-              style: { paddingTop: 5 },
-              removeClippedSubviews: false,
-              ref: tl => this.timeline = tl
-            }}
-            innerCircle={'icon'}
-            onEventPress={this.onEventPress}
-            separator={false}
-            detailContainerStyle={{ marginBottom: 20, paddingLeft: 5, paddingRight: 5, backgroundColor: "#BBDAFF", borderRadius: 10 }}
-            columnFormat='two-column'
-          />
+        <StatusBar hidden={true} />
+        <Timeline
+          style={styles.list}
+          data={this.data}
+          showTime={true}
+          circleSize={30}
+          circleColor='rgba(0,0,0,0)'
+          lineColor={this.state.timeColor}
+          timeContainerStyle={{ minWidth: 52 }}
+          timeStyle={{ textAlign: 'center', backgroundColor: this.state.timeColor, color: 'white', padding: 10, borderRadius: 50 }}
+          descriptionStyle={{ color: 'gray' }}
+          pageChanged={this.pageChanged.bind(this)}
+          options={{
+            style: { paddingTop: 5 },
+            removeClippedSubviews: false,
+            ref: tl => this.timeline = tl
+          }}
+          onEventPress={this.onEventPress}
+          detailContainerStyle={{ marginBottom: 10, paddingLeft: 10, paddingRight: 10, backgroundColor: this.state.containerColor, borderRadius: 20 }}
+          columnFormat='single-column-left'
+        />
       </View>
     );
   }
@@ -321,11 +336,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    padding: 10
   },
   list: {
     flex: 1,
-    marginTop: 20,
     backgroundColor: 'transparent'
   },
   title: {
